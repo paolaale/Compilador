322e1044df29336797fe14f28aa17efea9f75a7a
Paola Villarreal García <31547279+paolaale@users.noreply.github.com>
2021-04-03 14:12:32 -0600

    lexer completed and tested

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Compilador
+
+Febrero - Junio 2021
+
+Paola Villarreal García A00821971
+Alan Zavala Lévaro A01338448
---
+++ lexer.py
@@ -0,0 +1,83 @@
+import ply.lex as lex
+
+# Reserved word
+reserved = {
+    'if' : 'IF',
+    'then' : 'THEN',
+    'elif' : 'ELIF',
+    'else' : 'ELSE',
+    'int' : 'INT',
+    'float' : 'FLOAT',
+    'char' : 'CHAR',
+    'string' : 'STRING',
+    'void' : 'VOID',
+    'program' : 'PROGRAM',
+    'while' : 'WHILE',
+    'do': 'DO',
+    'from' : 'FROM',
+    'until' : 'UNTIL',
+    'inherits' : 'INHERITS',
+    'write' : 'WRITE',
+    'read' : 'READ',
+    'class' : 'CLASS',
+    'return' : 'RETURN'
+}
+
+# Tokens
+tokens = [
+    'CTEI', 'CTEF', 'CTESTRING', 'CTECHAR', 'ID',
+    'RELOP', 'PLUS', 'MINUS', 'DIVIDE', 'TIMES',
+    'EQUAL', 'TWOPOINTS', 'SEMICOLON', 'COMMA',
+    'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET',
+    'LBRACES', 'RBRACES'
+] + list(reserved.values())
+
+# Regular expression rules
+t_CTEI = r'[+-]?[0-9]+'
+t_CTEF = r'[+-]?([0-9]+)(\.)([0-9]+)?'
+t_CTESTRING = r'\".*?\"' 
+t_CTECHAR = r'\'.?\'' 
+t_RELOP = r'(<)|(>)|(>=)|(<=)|(==)|(!=)'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_DIVIDE = r'/'
+t_TIMES = r'\*'
+t_EQUAL = r'='
+t_TWOPOINTS = r'\:'
+t_SEMICOLON = r'\;'
+t_COMMA = r','
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LBRACES = r'\{'
+t_RBRACES = r'\}'
+
+# Ignore Characters
+t_ignore =' \t\n'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9_]*'
+    t.type = reserved.get(t.value,'ID') # Check for reserved words
+    return t
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build lexer
+lexer = lex.lex()
+
+""" # Test 
+data = ''' > + 4.5 * "abc" 3 , : ; 'a' int = -20 -709.89 ({)} A00821971 if do then # array[] inherits class $ .'''
+ 
+# Give the lexer some input
+lexer.input(data)
+ 
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break # No more input
+    print(tok)  """
