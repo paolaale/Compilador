bff036a36728205fc8d23c7d5c30a704483dcda0
AlanZavala <alan_fcb10@hotmail.com>
2021-06-01 20:04:45 -0700

    commented code in all missing .py files

--- MemoryAllocator.py
+++ MemoryAllocator.py
@@ -1,3 +1,4 @@
+# Class that has a dictionary of memories per instance
 class MemoryAllocator:
 
     def __init__(self):
--- Testing.py
+++ Testing.py
@@ -1,6 +1,9 @@
+# This file is only to test the classes, functions and vars that exist in a program
+# checking their scope and memory references. It is not used in the compiler creation.
+
 import semanticFunc as sF
 
-### Memory added to variable tables test ###
+# Function to get the memory reference assigned to a variable
 def printMemoryInDeclaration():
     print("insert num of vals to test memory: ");
     numOfVars = int(input())
@@ -15,7 +18,8 @@ def printMemoryInDeclaration():
         varName = input()
         print("memRef of Var ", sF.direcClasses.get(className).c_funcs.get(functName).f_vars[varName].memRef)
         i = i + 1
-
+        
+# Function to check the functions of a class
 def printFunctsTable():
     print("insert num of funcs tables to check: ")
     numOfVars = int(input())
@@ -29,6 +33,7 @@ def printFunctsTable():
         print("Funciontts of class: ", sF.direcClasses.get(className).c_funcs)
         i = i + 1
 
+# Function to check the variables of a function
 def printVarsTable():
     print("insert num of vars tables to check: ")
     numOfVars = int(input())
